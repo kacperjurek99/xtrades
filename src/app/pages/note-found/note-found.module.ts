0476c7997b9d185ce73d49215349f9939d7f7014
unknown <kjurek957@gmail.com>
2023-12-14 23:46:18 +0100

    first commit

---
+++ src/app/pages/note-found/note-found.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NoteFoundRoutingModule } from './note-found-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NoteFoundRoutingModule
+  ]
+})
+export class NoteFoundModule { }
